7ee8433ffe9c3079b3c86e145960718a1057ef17
arifrizqi <arifrizqy.ar15@gmail.com>
2023-05-13 14:08:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const firebase = require('firebase')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
